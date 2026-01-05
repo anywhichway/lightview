470f2c00343dc404b0ab69e5e87b067ff863ac9e
syblackwell <syblackwell@anywhichway.com>
2026-01-05 05:15:24 -0800

    componet ehancements and fixes

--- test-text-tag.js
+++
@@ -1,6 +0,0 @@
-
-import Lightview from './src/lightview.js';
-const { state, tags, effect } = Lightview;
-
-// Mock document for testing in Node if necessary, but this project seems to have tests.
-// Let's check how tests are run.
